f1941d8496ae2f12ebfa621e485fb14ccd709e71
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-03-29 19:36:56 -0400

    Update katas.js

--- katas.js
+++ katas.js
@@ -5,3 +5,15 @@ const bowlingPins = arr => "7 8 9 10\n 4 5 6 \n  2 3  \n   1   ".split`\n`.map(
       v.split` `.map(v => (arr.includes(v * 1) ? " " : v === "" ? "" : "I"))
         .join` `
   ).join`\n`;
+
+// kata 2
+
+const dismember = func => {
+    let a = func.toString().match(/\(.*\)/)[0]
+  if (a.length===2) return []
+  return a.slice(1,-1).split`,`.map(v=>v.trim())
+}
+
+// solution 2
+
+const dismember = func => (''+func).match(/(?<=\().*(?=\))/)[0].match(/\w+/g)||[];
